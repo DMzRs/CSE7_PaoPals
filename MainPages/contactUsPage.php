de0a3731a65575e37a0cd44ba40964afbd058bb4
DMrRs <dmrashidpferrer@gmail.com>
2025-02-05 18:34:45 +0800

    with feedback

---
+++ MainPages/contactUsPage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Styles/contactUsPage.css?v=<?php echo time(); ?>">
+    <title>PaoPals</title>
+</head>
+<body>
+    <?php include '../Templates/navBar.php'; ?>
+    <section class="mainContainer">
+        <div class="header">
+            <h1>Tell us your feedback!</h1>
+        </div>
+        <div class="first-container">
+            <div class="form-container">
+            <form action="">
+                <input class="inputs" type="text" id="name" name="name" placeholder="Your Name" required>
+                <input class="inputs" type="email" id="email" name="email" placeholder="Your Email" required>
+                <input class="inputs" type="text" id="subject" name="subject" placeholder="Subject" required>
+                <textarea class="inputs" rows="20" cols="50" id="feedback" name="feedback" placeholder="Enter your feedback" required></textarea>
+                <button class="submitBtn">Submit</button>
+            </form>
+            </div>
+            <div class="img-container">
+                <img src="../Images/Logo/chefContactUs_logo.png" alt="chef">
+            </div>
+        </div>
+        <div class="contactUs-container">
+            <div>
+                <h1>Contact Us</h1>
+            </div>
+            <div class="contactDetails-container">
+                <div class="hotline">
+                    <img src="" alt=""><h2>Hotline</h2>
+
+                    <h3>#8-44-44</h3>
+                </div>
+                <div class="email">
+                    <img src="" alt=""><h2>Email</h2>
+
+                    <h3>feedback@papls.com</h3>
+                </div>
+                <div class="address">
+                    <img src="" alt=""><h2>Address</h2>
+                    <h3>UM Matina, Davao City</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
--- Templates/navBar.php
+++ Templates/navBar.php
@@ -16,7 +16,7 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="menuPage.php">Menu</a></li>
-                    <li><a href="">Contact Us</a></li>
+                    <li><a href="contactUsPage.php">Contact Us</a></li>
                     <li><a href="">About Us</a></li>
                 </ul>
             </div>
