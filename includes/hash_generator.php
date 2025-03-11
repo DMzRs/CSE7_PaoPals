e0094a9c66ea5834d221c7084e645f3422743b28
90377Sednaaa <leanmurillo0201@gmail.com>
2025-03-11 17:48:56 +0800

    Fixes

--- includes/hash_generator.php
+++ includes/hash_generator.php
@@ -1,7 +1,12 @@
 <?php
 //Para rani sa admin password need hashing
 
-$plainTextPassword = 'jamesmendoza'; // Replace with your actual password
+$plainTextPassword = 'dmferrer'; // Replace with your actual password
+
+// DM = dmferrer
+// Mendoza = mendoza1234
+// Murillo = leanmurillo
+
 
 // Generate a hashed password using Bcrypt (default algorithm)
 $hashedPassword = password_hash($plainTextPassword, PASSWORD_DEFAULT);
